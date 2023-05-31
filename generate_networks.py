9ff2fb2f9beb6824a6d9dfefdef517bd29b05aee
Soumajit-Pramanik <soumajit.pramanik@gmail.com>
2023-05-31 17:50:02 +0530

    Add files via upload

--- generate_networks.py
+++ generate_networks.py
@@ -1,10 +1,13 @@
 #Generates synthetic networks
 import os, sys, time
 import subprocess
+from lfr_multilayer_v3 import create_layers
 
 new_dir = sys.argv[1]
 dir_lfr = sys.argv[2]
 
+if not os.path.exists(dir_lfr):
+	os.makedirs(dir_lfr)
 if not os.path.exists(new_dir):
 	os.makedirs(new_dir)
 if not os.path.exists(new_dir + "_networks"):
@@ -17,16 +20,29 @@ if not os.path.exists(new_dir + "_networks"):
 n=100
 k=6
 maxk=10
+nb_layers=2
 
 # config1
-list_alpha = [0.1, 0.2, 0.4, 0.5, 0.6, 0.8, 0.9]
+list_alpha = [0.1, 0.2]
 #list_alpha = [0.1, 0.2]
 list_mu = [0.05]
 #list_p = [1.0,0.95,0.9,0.85,0.8,0.75,0.7]
-list_p = [0.6,0.7,0.8,0.9]
+list_p = [0.6,0.7]
 list_p1 = [0.3]
 list_p2 = [0.0]
 
+mu=0.05
+
+intra_params={}
+intra_params['n']=n
+intra_params['k']=k
+intra_params['maxk']=maxk
+intra_params['mu']=0.05
+
+create_layers(dir_lfr, nb_layers, intra_params)
+
+#nb_com_layer , nb_link_layer = benchmark_lfr(0, dir_lfr, n, k, maxk, mu)
+#nb_com_layer , nb_link_layer = benchmark_lfr(1, dir_lfr, n, k, maxk, mu)
 
 # config2
 # list_alpha = [0.6]
@@ -88,8 +104,14 @@ for alpha in list_alpha:
 
 					i += 1
 					print "%i/%i" % (i, nb_iteration)
-
-					cmd = "python2.7 lfr_multilayer_v3.py %s %s -p %f -a %f -p1 %f -p2 %f" % (new_dir_p2, dir_lfr, p, alpha, p1, p2)
+					
+					cmd= "cp -r %s/layer0 %s" % (dir_lfr,new_dir_p2)
+					print cmd
+					os.popen(cmd)
+					cmd= "cp -r %s/layer1 %s" % (dir_lfr,new_dir_p2)
+					print cmd
+					os.popen(cmd)
+					cmd = "python2.7 lfr_multilayer_v3.py %s %s -p %f -a %f -p1 %f -p2 %f" % (new_dir_p2, new_dir_p2, p, alpha, p1, p2)
 					print cmd
 					#sys.exit(1)
 					os.popen(cmd)
